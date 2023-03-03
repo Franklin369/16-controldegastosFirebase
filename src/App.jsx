be8ed7fdb5182ddc24c6c69928d60d2ed20ac477
Franklin369 <codigo369.com@gmail.com>
2023-03-03 11:42:58 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { MyRoutes } from "./routers/routes";
+function App() {
+  return (
+    <>
+      <MyRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.config.jsx
@@ -0,0 +1,16 @@
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore"
+import {getAuth} from "firebase/auth"
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_APP_APIKEY,
+  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APP_APPID
+};
+
+const app = firebase.initializeApp(firebaseConfig)
+export const db = app.firestore();
+export const auth = getAuth(app);
